3eca06437a0c9a4ad77fa86a7d571665f624ec00
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2022-05-05 15:58:02 -0300

    Establece servidor de Express y testea registros con ThunderClient

--- server/models/car-rental-model.js
+++ server/models/car-rental-model.js
@@ -2,11 +2,11 @@ const db = require("../database/db.js");
 const { DataTypes } = require("sequelize");
 
 const rentalModel = db.define("rentals", {
-  id: { type: DataTypes.INTEGER },
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   pricePerDay: { type: DataTypes.INTEGER },
   totalPrice: { type: DataTypes.INTEGER },
   state: { type: DataTypes.STRING },
-  PaymentMethod: { type: DataTypes.STRING },
+  paymentMethod: { type: DataTypes.STRING },
   car: { type: DataTypes.STRING },
   user: { type: DataTypes.STRING },
 });
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,33 @@
 require("dotenv").config();
+const db = require("../database/db.js");
 const express = require("express");
-const configureDependencyInjection = require("./config/di.js");
+const cors = require("cors");
+
+const rentalRoutes = require("./routes/routes.js");
 
 const app = express();
-const port = procces.env.PORT || 3000;
+const port = 8000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
+app.use(cors());
+app.use(express.json());
+app.use("/rentals", rentalRoutes);
 
-const container = configureDependencyInjection(app);
-app.use(container.get("Session"));
+const conection = async () => {
+  try {
+    await db.authenticate();
+    console.log("Conexion exitosa a la db");
+  } catch (e) {
+    console.log(`Error de conexion: ${e}`);
+  }
+};
 
+// app.get("/", (req, res) => {
+//   res.send("Hola Mundo");
+// });
+
+app.listen(port, () => {
+  conection();
+  console.log(`Escuchando en el puerto: ${port}`);
+});
